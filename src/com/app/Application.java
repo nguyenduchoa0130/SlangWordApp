20743faba3bb2c588b7f6075398a08fece1218b0
hoa.nguyenduc <nguyenduchoa0130@gmail.com>
2022-07-08 00:41:42 +0700

    add options to menu

--- src/com/app/Application.java
+++ src/com/app/Application.java
@@ -6,17 +6,37 @@ import java.util.Scanner;
 
 public class Application {
   public static void printMenu() {
-    System.out.println("\n Menu:");
-    System.out.println("1. Look up a slang word.");
-    System.out.println("2. Print all slang words");
+
+    System.out.println("\n||================ MENU ================||");
+    System.out.println("1. Find meaning of slang word");
+    System.out.println("2. Find slang word based on definition");
     System.out.println("0. Exit application");
+    System.out.println("||======================================||");
   }
+
+  public static int selectOption() {
+    System.out.printf("");
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextInt();
+  }
+
   public static void run() {
     SlangWordLibrary slangWordLibrary = new SlangWordLibrary();
     int option;
     System.out.println("====|| WELCOME TO SLANG WORD APPLICATION ||====");
     while (true) {
       printMenu();
+      option = selectOption();
+      switch (option){
+        case ApplicationOption.OPT_EXIT_APP: {
+          System.out.println("See you!!");
+          return;
+        }
+        default: {
+          System.out.println("Please enter a existed option!!");
+          break;
+        }
+      }
     }
   }
 }
--- src/com/app/ApplicationOption.java
+++ src/com/app/ApplicationOption.java
@@ -2,6 +2,6 @@ package com.app;
 
 public class ApplicationOption {
   public final static int OPT_EXIT_APP = 0;
-  public final static int OPT_LOOKUP_A_SLANG_WORD = 1;
-  public final static int OPT_PRINT_ALL_SLANG_WORDS = 2;
+  public final static int OPT_FIND_MEANING_OF_SLANG_WORD = 1;
+  public final static int OPT_FIND_SLANG_WORD_BASED_ON_MEANING = 2;
 }
